4dc204df657cd40c831cb956be21a4ebcc9d2907
zay <114158079+zaythemonster@users.noreply.github.com>
2023-06-06 13:25:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const fs = require('fs-extra') 
-const path = require('path') 
+const fs = require("fs-extra") 
+const path = require("path") 
 
-const ver = path.join(process.env.LOCALAPPDATA, 'Roblox', 'Versions') 
+const ver = path.join(process.env.LOCALAPPDATA, "Roblox", "Versions") 
 
 const cya = fs.readdirSync(ver) 
 
 cya.forEach((yes) => {
-  const Roblox = path.join(ver, yes, 'PlatformContent', 'pc', 'textures') 
-
+  if (yes.includes("Studio")) {
+    return
+  }
+  
+  const Roblox = path.join(ver, yes, "PlatformContent", "pc", "textures") 
+  
   const Folders = ['brick', 'cobblestone', 'concrete', 'corrodedmetal', 'diamondplate', 'fabric', 'foil', 'glass', 'granite', 'grass', 'ice', 'marble', 'metal', 'pebble', 'plastic', 'sand', 'slate', 'water', 'wood', 'woodplanks'] 
-
+  
   Folders.forEach((folder) => {
-    const ThePath = path.join(Roblox, folder)
 
+    const ThePath = path.join(Roblox, folder) 
+    
     fs.remove(ThePath)
-      .then(() => {
-        console.log(`\x1b[35mSuccessfully Deleted Textures\x1b[0m:`)
-      })
-      .catch((err) => {
-        console.error(`The Folder ${ThePath} does not exist`, err)
-      }) 
+    .then(() => {
+      console.log(`Successfully Deleted Textures`) 
+    })
+    .catch((err) => {
+      console.error(`The Folder ${ThePath} does not exist`, err) 
+    }) 
   }) 
-}) 
+})
